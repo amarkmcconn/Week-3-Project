389e0a6fade0786f00fb724e6f2a50e1cbb654c2
Mark McConnell <amark.mcconnell@gmail.com>
2022-04-01 11:01:33 -0700

    4th  test of beepBoop() fx test complete.

--- README.md
+++ README.md
@@ -17,8 +17,8 @@ Test 4: "It should return an array of numbers from 0 to the user's inputted numb
 Code: beepBoop(5);
 Expected Output: [0, 1, 2, 3, 4, 5]
 
-Test 5: "It should return an array of numbers from 0 to the user's inputted number"
-Code: beepBoop(5);
-Expected Output: [0, 1, 2, 3, 4, 5]
+// Test 5: "It should return an array of numbers from 0 to the user's inputted number"
+// Code: beepBoop(5);
+// Expected Output: [0, 1, 2, 3, 4, 5]
 
 ```
--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,30 @@
 
 function beepBoop(input) {
   let arrayBeep = []
-  if (Number.isInteger(input)) {
-    const newNumber = input
+  if (Number.isInteger(input) === 0) {
+    return true
+    } else if (Number.isInteger(input) !== 0) {
+    const newNumber = parseInt(input)
     arrayBeep.push(newNumber)
-   } else {
-    return false
-   }
+    } else {
+    
+    }
+  console.log(arrayBeep) 
+}  
+
+
+function beepBoop(input) {
+  let arrayBeep = []
+  if (Number.isInteger(input) === 0) {
+    return true
+    } else if (Number.isInteger(input) !== 0) {
+    let newNumber = -1
+    for (let i = 0; i <= input; i++) {
+      newNumber++;
+      arrayBeep.push(newNumber)
+    }
+    } else {
+    return false  
+    }
   console.log(arrayBeep) 
 }  
