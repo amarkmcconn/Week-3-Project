6eb28cfc46e3293c6a817762ee20c96507e8f364
Mark McConnell <amark.mcconnell@gmail.com>
2022-04-01 11:20:41 -0700

    5th  test of beepBoop() fx test complete.

--- README.md
+++ README.md
@@ -17,8 +17,16 @@ Test 4: "It should return an array of numbers from 0 to the user's inputted numb
 Code: beepBoop(5);
 Expected Output: [0, 1, 2, 3, 4, 5]
 
-// Test 5: "It should return an array of numbers from 0 to the user's inputted number"
-// Code: beepBoop(5);
-// Expected Output: [0, 1, 2, 3, 4, 5]
+Test 5: "It should return the number 1 as beep"
+Code: beepBoop(5);
+Expected Output: [0, "beep", 2, 3, 4, 5]
+
+Test 6: "It should return the number 1 as beep and 2 as boop"
+Code: beepBoop(5);
+Expected Output: [0,"beep", "boop", 3, 4, 5]
+
+Test 6: "It should return the number 1 as beep and 2 as boop"
+Code: beepBoop(5);
+Expected Output: [0,"beep", "boop", 3, 4, 5]
 
 ```
--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,6 @@ function beepBoop(input) {
   if (Number.isInteger(input) === 0) {
     return true
     } else if (Number.isInteger(input) !== 0) {
-    const newNumber = parseInt(input)
-    arrayBeep.push(newNumber)
-    } else {
-    
-    }
-  console.log(arrayBeep) 
-}  
-
-
-function beepBoop(input) {
-  let arrayBeep = []
-  if (Number.isInteger(input) === 0) {
-    return true
-    } else if (Number.isInteger(input) !== 0) {
     let newNumber = -1
     for (let i = 0; i <= input; i++) {
       newNumber++;
@@ -28,5 +14,28 @@ function beepBoop(input) {
     } else {
     return false  
     }
+  if (arrayBeep[1] === 1){
+    const index = arrayBeep.indexOf(1)
+    arrayBeep[index] = "beep"
+    } else if (arrayBeep[]) {
+
+    }
   console.log(arrayBeep) 
-}  
+} 
+
+
+ 
+
+
+// function beepBoop(input) {
+//   let arrayBeep = []
+//   if (Number.isInteger(input) === 0) {
+//     return true
+//     } else if (Number.isInteger(input) !== 0) {
+//     const newNumber = parseInt(input)
+//     arrayBeep.push(newNumber)
+//     } else {
+    
+//     }
+//   console.log(arrayBeep) 
+// }  
